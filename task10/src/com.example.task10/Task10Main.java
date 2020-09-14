490a8f33f67e0aa21879fa1f10ad17a44e0b92ab
Pavel Ryabinin <ryabinin_pavel@f-lab.tech>
2020-09-14 16:00:16 +0500

    Task10 [Рябинин]

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,15 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minValue = Integer.MAX_VALUE;
+        int minIndex = 0;
+        for(int i = 0; i < arr.length; ++i) {
+            if (arr[i] <= minValue) {
+                minIndex = i;
+                minValue = arr[i];
+            }
+        }
+        return minIndex;
     }
 
 }
