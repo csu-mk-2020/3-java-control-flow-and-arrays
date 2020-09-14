e90e78c906b934d35437b630b2f5ae70342bb076
Pavel Ryabinin <ryabinin_pavel@f-lab.tech>
2020-09-14 16:06:13 +0500

    Task11 [Рябинин]

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -4,15 +4,24 @@ public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+        int[] arr = new int[]{9, 11, 7, 8};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int minIndex = 0;
+        for(int i = 1; i < arr.length; ++i) {
+            if (arr[i] < arr[minIndex]) {
+                minIndex = i;
+            }
+        }
+        int tmp = arr[0];
+        arr[0] = arr[minIndex];
+        arr[minIndex] = tmp;
     }
 
 }
