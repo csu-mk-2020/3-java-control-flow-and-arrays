515323c1e4e8319a3cb4175019898bff2d225131
funin <funin99@bk.ru>
2020-09-19 19:45:56 +0500

    task09 [Фунин]

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,14 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length;i++) {
+            if (min > arr[i]){
+                min = arr[i];
+            }
+        }
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return min;
     }
 
 }
