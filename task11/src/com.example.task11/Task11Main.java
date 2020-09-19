eb572dbdb637d5afcbb8cfd1ef311eb7bf94216c
redrockstyle <ulowpie@gmail.com>
2020-09-19 16:16:22 +0500

    [Khazov]

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,25 @@
 package com.example.task11;
 
+import java.util.stream.IntStream;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+
+        int[] arr = {7, 5, 10, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr.length != 0) {
+            //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+            int ind = IntStream.range(0, arr.length).reduce((i, j) -> arr[i] >= arr[j] ? j : i).getAsInt();
+            arr[0] = arr[0] + arr[ind] - (arr[ind] = arr[0]);
+        }
+        return;
     }
 
 }
