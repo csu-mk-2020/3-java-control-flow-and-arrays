630e8a658493ce3b05005a52700f7b2de1e05623
redrockstyle <ulowpie@gmail.com>
2020-09-19 17:14:36 +0500

    [Khazov]

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -1,19 +1,22 @@
 package com.example.task13;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+        System.out.println(java.util.Arrays.toString(removeMoreThen1000(arr)));
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null) return null;
+        else if (arr.length == 0) return new int[]{};
+        else return Arrays.stream(arr).filter(i -> i < 1000).toArray();
     }
 
 }
