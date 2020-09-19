89607b18cbec02bf7240d45516b8b98d4e702707
funin <funin99@bk.ru>
2020-09-19 14:51:54 +0500

    task02 [Фунин]

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -10,6 +10,18 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber == 1 || monthNumber == 2 || monthNumber == 12) {
+            return "зима";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        }
+        else if (monthNumber == 3 || monthNumber == 4 || monthNumber == 5) {
+            return "весна";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        }
+        else if (monthNumber == 6 || monthNumber == 7 || monthNumber == 8) {
+            return "лето";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        }
+        else if (monthNumber == 9 || monthNumber == 10 || monthNumber == 11) {
+            return "осень";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        }
+        return " ";
     }
 }
