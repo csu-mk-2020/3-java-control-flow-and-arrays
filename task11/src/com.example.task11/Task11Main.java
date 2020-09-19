0011961220f881c12df5022dc21a82d8f193cd0b
funin <funin99@bk.ru>
2020-09-19 20:17:52 +0500

    task11 [Фунин]

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,31 @@
 package com.example.task11;
 
+import java.lang.reflect.Array;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+
+        int[]  arr = new int[]{};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length != 0) {
+        int min = arr[0];
+        int buff = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (min >= arr[i] && i != 0) {
+                    min = arr[i];
+                    buff = arr[0];
+                    arr[0] = arr[i];
+                    arr[i] = buff;
+                }
+            }
+        }
     }
-
+    //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
 }
