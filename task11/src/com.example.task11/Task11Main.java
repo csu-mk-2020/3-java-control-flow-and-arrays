bbd087f7fe36840629c3e0e468baa2d8ce5cdda7
redrockstyle <ulowpie@gmail.com>
2020-09-19 17:22:04 +0500

    [Khazov]

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -14,7 +14,7 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        if (arr.length != 0) {
+        if (arr != null && arr.length != 0) {
             //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
             int ind = IntStream.range(0, arr.length).reduce((i, j) -> arr[i] >= arr[j] ? j : i).getAsInt();
             arr[0] = arr[0] + arr[ind] - (arr[ind] = arr[0]);
