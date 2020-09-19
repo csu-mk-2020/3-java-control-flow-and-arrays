389e5d58e1abd6d7dc68ad958ba09b4b37df2f90
funin <funin99@bk.ru>
2020-09-20 00:21:49 +0500

    task12 [Фунин]

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -4,15 +4,30 @@ public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        if (arr != null && arr.length != 0) {
+            boolean isSorted = false;
+            int buf;
+            while (!isSorted) {
+                isSorted = true;
+                for (int i = 0; i < arr.length - 1; i++) {
+                    if (arr[i] > arr[i + 1]) {
+                        isSorted = false;
 
+                        buf = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = buf;
+                    }
+                }
+            }
+            //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        }
+    }
 }
