de4fa6ba46119bad75fbbade24793c0373828364
Kirito1899 <alex-kadykov@bk.ru>
2020-09-20 17:08:37 +0500

    Task08 [Кадыков]

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -14,7 +14,14 @@ public class Task08Main {
 
     static long mult(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long pr = 1;
+        if (arr.length < 1) {
+            return 0;
+        }
+        for (int i : arr) {
+            pr *= i;
+        }
+        return pr;
     }
 
 }
