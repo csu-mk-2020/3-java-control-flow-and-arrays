85c1017b744271bc27aa99efc7cfd56a6f74e53a
Kirito1899 <alex-kadykov@bk.ru>
2020-09-20 16:26:07 +0500

    Task02 [Кадыков]

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -10,6 +10,7 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] Seasons = new String[]{"зима", "весна", "лето", "осень"};
+        return Seasons[(monthNumber % 12) / 3];//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
