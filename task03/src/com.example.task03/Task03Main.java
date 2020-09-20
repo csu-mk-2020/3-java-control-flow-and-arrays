df0522101fcfb3c3f6167e90efc537f72caf8593
Kirito1899 <alex-kadykov@bk.ru>
2020-09-20 16:34:37 +0500

    Task03 [Кадыков]

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -4,12 +4,17 @@ public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        System.out.println(getNameOfWeekDays(7));
-         */
+
+        System.out.println(getNameOfWeekDays(13));
+
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] Days = new String[]{"такого дня недели не существует", "понедельник",
+                "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+        if (weekDaysNumber > 7 | weekDaysNumber < 1) {
+            return Days[0];
+        }
+        return Days[weekDaysNumber];//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
