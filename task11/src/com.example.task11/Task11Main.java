f2d9a50d6ea6b7797fe0c658a9965135de34293f
Petrov <qwe@qwe.com>
2020-09-24 20:46:10 +0500

    Task11 [Теплинский]

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,33 @@
 package com.example.task11;
 
+import sun.security.util.ArrayUtil;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+        int[] arr = null;
         swap(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int min;
+        try {
+            min = Arrays.stream(arr).min().getAsInt();
+        } catch (NoSuchElementException | NullPointerException ex) {
+            return;
+        }
+
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == min) {
+                index = i;
+            }
+        }
+        int tmp = arr[0];
+        arr[0] = arr[index];
+        arr[index] = tmp;
     }
 
 }
