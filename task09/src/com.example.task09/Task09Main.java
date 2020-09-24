8c3b1fc105d9e186acdcdb44e84bf80cf00da5f0
FinnLEA <56691691+FinnLEA@users.noreply.github.com>
2020-09-24 15:13:43 +0500

    Веденькин Task09

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -1,5 +1,7 @@
 package com.example.task09;
 
+import java.util.Arrays;
+
 public class Task09Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,11 +13,18 @@ public class Task09Main {
         arr[2] = 3;
         System.out.println(min(arr));
          */
+        System.out.println(min(new int[]{5, 4, 7, 2, 8, 3, 6}));
     }
 
     static int min(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = arr[0];
+        for(int el : arr){
+            if(el < min){
+                min = el;
+            }
+        }
+        return min;
     }
 
 }
