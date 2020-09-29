212ccd003d6f05fe5934a449b3751f087963b4e8
Astaroth <avm725@gmail.com>
2020-09-30 02:31:25 +0500

    Task10[Мысливко]

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,18 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
+        /*
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         return 0;
+        */
+        int min = 2147483647;
+        int num = 0;
+        for(int i =0; i < arr.length; ++i)
+            if(arr[i] <= min){
+                min = arr[i];
+                num = i;
+            }
+        return num;
     }
 
 }
