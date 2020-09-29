8e61138dcfd654f9ecb54257f23e8787d0dc3f9f
Astaroth <avm725@gmail.com>
2020-09-30 02:20:11 +0500

    Task06[Мысливко]

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -10,8 +10,11 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
+        /*
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         return 0;
+        */
+        return Math.max(Math.max(Math.max(c, d), b), a);
     }
 
 }
