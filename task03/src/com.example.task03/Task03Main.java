04e5caf80a3b055da210682d01e5bef13fa02a1d
Виталий Вересников <Vitosiy21@yandex.ru>
2020-10-02 11:10:23 +0500

    Task03 [Veresnikov]

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -3,11 +3,28 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
 
-        System.out.println(getNameOfWeekDays(7));
+        System.out.println(getNameOfWeekDays(-60));
 
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return new String[]{"такого дня недели не существует", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"}[weekDaysNumber];
+        switch (weekDaysNumber) {
+            case 1:
+                return "понедельник";
+            case 2:
+                return "вторник";
+            case 3:
+                return "среда";
+            case 4:
+                return "четверг";
+            case 5:
+                return "пятница";
+            case 6:
+                return "суббота";
+            case 7:
+                return "воскресенье";
+            default:
+                return "такого дня недели не существует";
+        }
     }
 }
