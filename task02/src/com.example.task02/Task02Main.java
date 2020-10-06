e8c10418a60f60803646c436dea93ed2927430de
Shinen <Fixmaks99@gmail.com>
2020-10-06 14:55:29 +0500

    task02 [Ефимов]

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -10,9 +10,9 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        if (monthNumber >= 3 && monthNumber <= 5) {return "весна";}
-        if (monthNumber >= 6 && monthNumber <= 8) {return "лето";}
-        if (monthNumber >= 9 && monthNumber <=11) {return "осень";}
+        if (monthNumber >= 3 && monthNumber <= 5) return "весна";
+        if (monthNumber >= 6 && monthNumber <= 8) return "лето";
+        if (monthNumber >= 9 && monthNumber <=11) return "осень";
         return "зима";
     }
 }
